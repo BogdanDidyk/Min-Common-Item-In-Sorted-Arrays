b68621a4e1d36c8a1faccd18dfceeceac0b38c31
Bogdan Didyk <bogdandidyk26@gmail.com>
2023-04-03 13:16:54 +0300

    Added the function that implements the algorithm

--- script.js
+++ script.js
@@ -8,6 +8,21 @@ function getRandomSortedArray(length, min = 0, max = 9) {
     return arr.sort((a, b) => a - b);
 }
 
+function getMinCommonItem(arr1, arr2, arr3) {
+    const len1 = arr1.length, len2 = arr2.length, len3 = arr3.length;
+    let i = 0, j = 0, k = 0;
+
+    while (i < len1 && j < len2 && k < len3) {
+        if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) return arr1[i];
+
+        if (arr1[i] <= arr2[j] && arr1[i] <= arr3[k]) i++;
+        else if (arr2[j] <= arr1[i] && arr2[j] <= arr3[k]) j++;
+        else if (arr3[k] <= arr1[i] && arr3[k] <= arr2[j]) k++;
+    }
+
+    return undefined;
+}
+
 const arr1 = getRandomSortedArray(10);
 const arr2 = getRandomSortedArray(8);
 const arr3 = getRandomSortedArray(9);
@@ -15,3 +30,6 @@ const arr3 = getRandomSortedArray(9);
 console.log(arr1);
 console.log(arr2);
 console.log(arr3);
+
+const minCommonItem = getMinCommonItem(arr1, arr2, arr3);
+console.log("Min common item:", minCommonItem);
